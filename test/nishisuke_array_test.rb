124948338d7696c31c3b9ab98daf4bf68b9c2f1e
nishisuke <nishisuke13+git@gmail.com>
2018-02-28 11:43:14 +0900

    Add test

---
+++ test/nishisuke_array_test.rb
@@ -0,0 +1,11 @@
+require_relative "test_helper"
+
+class NishisukeArrayTest < Minitest::Test
+  def test_that_it_has_a_version_number
+    refute_nil ::NishisukeArray::VERSION
+  end
+
+  def test_it_does_something_useful
+    assert false
+  end
+end
